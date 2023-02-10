92c24f6d355f7e4e370ef72c6e4f7e009ab2127b
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-02-10 19:36:30 +0530

    Added Prettier

--- src/components/UI/Card.tsx
+++ src/components/UI/Card.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 
 const cardStyling = {
-    backgroundColor: '#505050',
-    padding: '10px',
-    borderRadius: '10px',
-    marginBottom: '10px'
+  backgroundColor: "#505050",
+  padding: "10px",
+  borderRadius: "10px",
+  marginBottom: "10px",
 } as const;
 
-export const Card: React.FC<{children: React.ReactNode}> = (props) => {
-    return (
-        <div style={cardStyling}>
-            {props.children}
-        </div>
-    )
-}
+export const Card: React.FC<{ children: React.ReactNode }> = (props) => {
+  return <div style={cardStyling}>{props.children}</div>;
+};
--- src/components/UI/ErrorModal.tsx
+++ src/components/UI/ErrorModal.tsx
@@ -2,7 +2,11 @@ import React, { MouseEventHandler } from "react";
 import { Card } from "./Card";
 import classes from "./ErrorModal.module.css";
 
-export const ErrorModal: React.FC<{title: string, message: string, closeDialogue: () => void}> = (props) => {
+export const ErrorModal: React.FC<{
+  title: string;
+  message: string;
+  closeDialogue: () => void;
+}> = (props) => {
   return (
     <div className={classes.background} onClick={props.closeDialogue}>
       <div className={classes.card}>
